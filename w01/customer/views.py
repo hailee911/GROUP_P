c7b9f19b39dca343f24bc85f70a2d40726198a43
hyundingi <qo0723@naver.com>
2024-12-06 18:32:45 +0900

    1206 현지

--- w01/customer/views.py
+++ w01/customer/views.py
@@ -3,10 +3,12 @@ from customer.models import NoticeBoard
 
 # Create your views here.
 def main(request):
-  return render(request, 'customer_service.html')
+  qs = NoticeBoard.objects.order_by('-bno')[:5]
+  context = {'notice_5':qs}
+  return render(request, 'customer_service.html', context)
 
 def noticelist(request):
-  qs = NoticeBoard.objects.all()
+  qs = NoticeBoard.objects.all().order_by('-bno')
   context = {'notice_list':qs}
   return render(request, 'customer_notice_list.html', context)
 
