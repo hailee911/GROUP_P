bfae0260014e70b8af76eb17949ba86531de8a51
yjkim333 <kyjoon0105@gmail.com>
2024-12-10 18:10:51 +0900

    1210

--- w01/diary/models.py
+++ w01/diary/models.py
@@ -5,7 +5,6 @@ class DiaryBoard(models.Model):
   dno = models.AutoField(primary_key=True)
   member = models.ForeignKey(Member,on_delete=models.DO_NOTHING, null=True)
   ## Board객체 : 좋아요 체크 - dno,member
-  like_members = models.ManyToManyField(Member,default='', related_name='diary_like_member')
   dtitle = models.CharField(max_length=1000)
   dcontent = models.TextField()
   dhit = models.IntegerField(default=0)
