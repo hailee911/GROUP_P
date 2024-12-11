56cc831f6dce3c3cf68cb4aad1b612091412edc5
sarah3832 <sarah38320@gmail.com>
2024-12-11 18:29:22 +0900

    d

--- w01/diary/admin.py
+++ w01/diary/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from diary.models import DiaryBoard
+from diary.models import MdiaryBoard
 
-@admin.register(DiaryBoard)
-class DiaryBoardAdmin(admin.ModelAdmin):
-  list_display = ['dno','dtitle','ddate']
+@admin.register(MdiaryBoard)
+class MdiaryBoardAdmin(admin.ModelAdmin):
+    list_display = ['mno','id','get_nicName', 'mtitle', 'mdate']
+
+    # 커스텀 메서드 정의
+    def get_nicName(self, obj):
+        return obj.id.nicName if obj.id and hasattr(obj.id, 'nicName') else None
+
+    # list_display에서 보이는 이름 설정
+    get_nicName.short_description = '닉네임'
   
+
 
 # 우체통
 from diary.models import Letter
--- w01/diary/views.py
+++ w01/diary/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from diary.models import DiaryBoard
 from loginpage.models import Member
 from diary.models import Letter
 from loginpage.models import Member
@@ -17,13 +16,13 @@ def diaryHome(request):
   return render(request,'diaryHome.html', context)
 
 
+
+
+## 가족다이어리 생성
+def diaryMake(requset):
+  return render(requset,'diaryMake.html')
+
+
 ## 내 다이어리 목록
 def MdiaryList(request):
-  qs = DiaryBoard.objects.all()
-  for i in qs:
-    print(i.diarytit)
-    print(i.dtitle)
-    print(i.dcontent)
-    print('-=' * 39)
-  context = {"MdiaryList":qs}
-  return render(request,'MdiaryList.html',context)
+  return render(request,'MdiaryList.html')
