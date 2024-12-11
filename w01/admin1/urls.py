6b10d9cc86a1dcadc048f335084d4b6df22e8cce
yjkim333 <kyjoon0105@gmail.com>
2024-12-11 16:35:27 +0900

    as

--- w01/admin1/urls.py
+++ w01/admin1/urls.py
@@ -11,4 +11,35 @@ urlpatterns = [
     path('admin_memList/', views.admin_memList, name="admin_memList"),
 	# 관리자리스트
     path('admin_adminList/', views.admin_adminList, name="admin_adminList"),
+<<<<<<< Updated upstream
+=======
+	# 관리자추가
+    path('admin_adminAdd/', views.admin_adminAdd, name="admin_adminAdd"),
+	# 관리자상세정보페이지
+    path('admin_adminView/<str:id>/', views.admin_adminView, name="admin_adminView"),
+	# 관리자정보수정페이지
+    path('admin_adminUpdate/<str:id>/', views.admin_adminUpdate, name="admin_adminUpdate"),
+	# 관리자삭제
+    path('admin_adminDelete/<str:id>/', views.admin_adminDelete, name="admin_adminDelete"),
+	# 관리자삭제(여러명 동시 삭제)
+    path('admin_adminsDelete/', views.admin_adminsDelete, name="admin_adminsDelete"),
+	# 공지사항리스트
+    path('admin_noticeList/', views.admin_noticeList, name="admin_noticeList"),
+	# 공지사항쓰기
+    path('admin_notiWrite/', views.admin_notiWrite, name="admin_notiWrite"),
+	# 포스트리스트
+    path('admin_postList/', views.admin_postList, name="admin_postList"),
+	# 유저상세정보페이지
+    path('admin_memView/<str:id>/', views.admin_memView, name="admin_memView"),
+	# 유저정보수정페이지
+    path('admin_memUpdate/<str:id>/', views.admin_memUpdate, name="admin_memUpdate"),
+	# 유저삭제
+    path('admin_memDelete/<str:id>/', views.admin_memDelete, name="admin_memDelete"),
+	# 유저삭제(여러명 동시 삭제)
+    path('admin_memsDelete/', views.admin_memsDelete, name="admin_memsDelete"),
+	# 유저추가페이지
+    path('admin_memAdd/', views.admin_memAdd, name="admin_memAdd"),
+
+
+>>>>>>> Stashed changes
 ]
